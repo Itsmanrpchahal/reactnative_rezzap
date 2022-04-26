17abdd304e69972c494fe66c5e38e36139e4fa73
Pardip Bhatti <sandeepk9187@gmail.com>
2022-04-26 15:32:42 +0530

    Messages Changes

--- src/screens/private/messages/index.tsx
+++ src/screens/private/messages/index.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { withTheme } from "styled-components";
 // @ts-ignore
 import styled from "styled-components/native";
-import { FlatList, TextInput, TouchableOpacity } from "react-native";
+import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { demoImage, dp, search, close } from "@root/utils/assets";
 import { useActions } from "@root/hooks/useActions";
 import { useTypedSelector } from "@root/hooks/useTypedSelector";
@@ -11,8 +11,8 @@ import { useIsFocused, useTheme } from "@react-navigation/native";
 import { NotFound } from "@root/utils/globalStyle";
 import NavigationStrings from "@root/navigation/navigationStrings";
 import { imageUrl } from "@root/utils/constants";
-import { MainWrapper } from "@root/utils/globalStyle";
 import AppLoader from "@root/components/Loader";
+import formatDistance from "date-fns/formatDistance";
 
 const Messages = (props: any) => {
   const isFocused = useIsFocused();
@@ -26,12 +26,14 @@ const Messages = (props: any) => {
       getMessageList();
     }
   }, [isFocused]);
+
+
   // @ts-ignore
   return (
-    <MainWrapper>
+    <MainView>
 
-      <SearchWrapper>
-        <TextInput style={{color:colors.black}} placeholder={"Search"} onChangeText={(mess) => setSearch(mess)} >
+      <SearchWrapper style={[styles.shadowBottonContainerStyle]}>
+        <TextInput style={{ color: colors.black }} placeholder={"Search List"} onChangeText={(mess) => setSearch(mess)} >
 
 
         </TextInput>
@@ -54,13 +56,12 @@ const Messages = (props: any) => {
         ) :
           <ParentWrapper>
 
-
-            <Divider />
             {
               loading ? (
                 <NotFound>Loading...</NotFound>
               ) :
                 <FlatList
+                  style={{ height: '100%' }}
                   nestedScrollEnabled={true}
                   data={messageListData.data}
                   numColumns={1}
@@ -71,36 +72,37 @@ const Messages = (props: any) => {
                         props.navigation.navigate(NavigationStrings.MESSAGE_DETAIL, { id: item.id });
                       }}>
                         <ChatMainView>
-
                           <ChatWrapper>
-                            <ProfileImage source={item != null ? { uri: imageUrl + item.profile_photo } : demoImage} />
+                            <ProfileImageCont>
+                              <ProfileImage source={item != null ? { uri: imageUrl + item.profile_photo } : demoImage} />
+                            </ProfileImageCont>
                             <ChatNameView>
                               <ChatTimeNameWrapper>
                                 <ChatText numberofLines={1}>
                                   {item.name}
                                 </ChatText>
-                                
-                              </ChatTimeNameWrapper>
+                               
+                                {/* <TimeAgoTV>{formatDistance(new Date(new Date(item.created).getFullYear(), new Date(item.created).getMonth(), new Date(item.created).getDate()),
+                                  new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()),
+                                 )}</TimeAgoTV> */}
 
+                              </ChatTimeNameWrapper>
                               <ChatMessage color={colors.black} fontWeight={500}>
                                 {item.message}
                               </ChatMessage>
-                             
-                              {/*}*/}
+
                             </ChatNameView>
                           </ChatWrapper>
-                          <Divider />
+                          
                         </ChatMainView>
                       </TouchableOpacity>
                     );
                   }}
                 />
             }
-
-
           </ParentWrapper>
       }
-    </MainWrapper>
+    </MainView>
 
 
   );
@@ -113,10 +115,8 @@ type TextProps = {
   fontWeight: string;
 };
 
-const Divider = styled.View`
-  height: 1px;
-
-  background-color: ${({ theme }: any) => theme.colors.borderGray};
+const TimeAgoTV = styled.Text`
+color:#C7C7C7;
 `;
 
 const ChatMessage = styled.Text<TextProps>`
@@ -125,8 +125,8 @@ const ChatMessage = styled.Text<TextProps>`
 `;
 
 const ChatTimeNameWrapper = styled.View`
-  flex-direction: row;
-  width: 80%;
+  flex-direction:row;
+  width: 70%;
   justify-content: space-between;
 `;
 
@@ -141,11 +141,23 @@ const ChatNameView = styled.View`
   margin-left: 16px;
 `;
 
-const ChatMainView = styled.View``;
-
+const ChatMainView = styled.View`
+padding-right:25px;
+border-color:#C7C7C7;
+border-top-width:0.5px;
+border-bottom-width:0.5px;
+`;
+const ProfileImageCont = styled.View`
+  background-color:#9AD3F4;
+  padding:4px;
+  border-radius:50px;
+  width:42px;
+  height:42px;
+`;
 const ProfileImage = styled.Image`
-  width: 45px;
-  height: 45px;
+  width: 34px;
+  height: 34px;
+  border:2px solid #ffffff;
   border-radius: 25px;
 `;
 const ChatWrapper = styled.View`
@@ -156,18 +168,33 @@ const ChatWrapper = styled.View`
 
 const ImageView = styled.Image``;
 
+const styles = StyleSheet.create({
+  shadowBottonContainerStyle: {    //<--- Style without elevation
+    borderWidth: 0,
+    borderRadius: 20,
+    borderColor: '#fff',
+    borderBottomWidth: 2,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+  },
+
+})
+
 const SearchWrapper = styled.View`
   width: auto;
   margin: 16px;
   border-radius: 20px;
   border-width: 1px;
-  padding-left: 10px;
+  padding-left: 20px;
   padding-right:10px;
   height:40px;
-  border-color: ${({ theme }: any) => theme.colors.borderGray};
+  border:none;
   justify-content: space-between;
   align-items:center;
   flex-direction: row;
+  background-color:#ffffff
 `;
 
 const ParentWrapper = styled.View`
@@ -175,8 +202,11 @@ const ParentWrapper = styled.View`
   height: auto`
   ;
 
+const MainView = styled.View`
+background-color:#FFFFFF
+height:100%;`;
 
-function alert(searcht: string): React.ReactNode {
+function moment(arg0: string) {
   throw new Error("Function not implemented.");
 }
 
--- src/screens/private/messages/messageDetail.tsx
+++ src/screens/private/messages/messageDetail.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import {  NotFound,MainWrapper } from "@root/utils/globalStyle";
+import { NotFound, MainWrapper } from "@root/utils/globalStyle";
 import { withTheme } from "styled-components";
-import { FlatList, Text, TextInput, TouchableOpacity } from "react-native";
+import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity } from "react-native";
 import { useIsFocused, useTheme } from "@react-navigation/native";
 import { useActions } from "@root/hooks/useActions";
 import { useTypedSelector } from "@root/hooks/useTypedSelector";
@@ -11,14 +11,15 @@ import styled from "styled-components/native";
 import { format } from "date-fns";
 import { imageUrl } from "@root/utils/constants";
 import AppLoader from "../../../components/Loader";
+import formatDistance from "date-fns/fp/formatDistance";
 
 // @ts-ignore
 const MessageDetail = ({ props, route }) => {
   const { colors }: any = useTheme();
   const isFocused = useIsFocused();
-  const { getMessageDetail ,sendMessage} = useActions();
+  const { getMessageDetail, sendMessage } = useActions();
   const { messageListData, loading } = useTypedSelector((state) => state.messageListData);
-  const [message,setMessageTV] = useState('');
+  const [message, setMessageTV] = useState('');
 
 
   useEffect(() => {
@@ -28,54 +29,60 @@ const MessageDetail = ({ props, route }) => {
   }, [isFocused]);
 
   return (
-    <MainWrapper>
+    <ParentWrapper>
       {
         loading ? (
-           <AppLoader/>
-          ) :
+          <AppLoader />
+        ) :
           <FlatList
             nestedScrollEnabled={true}
             data={messageListData.data}
+            showsHorizontalScrollIndicator={false}
             numColumns={1}
             inverted={true}
             horizontal={false}
 
             renderItem={({ item }) => {
               return (
-                  <ChatMainView>
+                <ChatMainView>
 
-                    <ChatWrapper flexDirection= { item.message_type === 'sender' ?'row-reverse' :'row'}>
-                      <ProfileImage source={messageListData.user_data && Object.keys(messageListData.user_data).length > 0 ? { uri: item.message_type != 'sender'? imageUrl + messageListData.user_data.profile_photo :imageUrl+ messageListData.supporter_data.profile_photo} : demoImage} />
-                      <ChatNameView>
-                        <ChatTimeNameWrapper>
+                  <ChatWrapper flexDirection={item.message_type === 'sender' ? 'row-reverse' : 'row'}>
+                    <ProfileImage source={messageListData.user_data && Object.keys(messageListData.user_data).length > 0 ? { uri: item.message_type != 'sender' ? imageUrl + messageListData.user_data.profile_photo : imageUrl + messageListData.supporter_data.profile_photo } : demoImage} />
+                    <ChatNameView>
+                      <ChatTimeNameWrapper>
 
-                          <ChatText flexDirection={ item.message_type === 'sender' ?'right' :'left'}>
-                            {messageListData.user_data && Object.keys(messageListData.user_data).length > 0 ? item.message_type != 'sender'?  messageListData.user_data.first_name : messageListData.supporter_data.first_name :''}
-                          </ChatText>
-
-                        </ChatTimeNameWrapper>
-                        <ChatMessage flexDirection={ item.message_type === 'sender' ?'right' :'left'}>
-                          {item.message}
-                        </ChatMessage>
-                      </ChatNameView>
-                    </ChatWrapper>
-                  </ChatMainView>
+                        <ChatText flexDirection={item.message_type === 'sender' ? 'right' : 'left'}>
+                          {messageListData.user_data && Object.keys(messageListData.user_data).length > 0 ? item.message_type != 'sender' ? messageListData.user_data.first_name : messageListData.supporter_data.first_name : ''}
+                        </ChatText>
+                        {/* {
+                          item.message_type !== 'sender' && <TimeAgoTV>{formatDistance(new Date(new Date(messageListData.user_data.created).getFullYear(), new Date(messageListData.user_data.created).getMonth(), new Date(messageListData.user_data.created).getDate()),
+                            new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()),
+                           )}</TimeAgoTV>
+                        } */}
+                  
+                      </ChatTimeNameWrapper>
+                      <ChatMessage flexDirection={item.message_type === 'sender' ? 'right' : 'left'}>
+                        {item.message}
+                      </ChatMessage>
+                    </ChatNameView>
+                  </ChatWrapper>
+                </ChatMainView>
               );
             }}
           />
       }
 
       <SendMessageWrapper>
-
-        <SearchWrapper>
-          <TextInput style={{color:colors.black,padding:10}} placeholder={"Type message"} onChangeText={(mess) => setMessageTV(mess)} value={message}>
-          </TextInput> 
+        <ProfileImage source={demoImage} />
+        <SearchWrapper style={[styles.shadowBottonContainerStyle]}>
+          <TextInput style={{ color: colors.black, padding: 10 }} placeholder={"Type message"} onChangeText={(mess) => setMessageTV(mess)} value={message}>
+          </TextInput>
 
           <TouchableOpacity onPress={async () => {
-              sendMessage({
-                'supporter_id': route.params.id,
-                'message': message
-              })
+            sendMessage({
+              'supporter_id': route.params.id,
+              'message': message
+            })
             setMessageTV('')
           }}>
             <ImageView source={send} />
@@ -83,7 +90,7 @@ const MessageDetail = ({ props, route }) => {
         </SearchWrapper>
       </SendMessageWrapper>
 
-    </MainWrapper>
+    </ParentWrapper>
   );
 };
 
@@ -91,7 +98,7 @@ const MessageDetail = ({ props, route }) => {
 export default withTheme(MessageDetail);
 
 type TextProps = {
-  flexDirection:string
+  flexDirection: string
 };
 
 const ImageView = styled.Image`
@@ -99,16 +106,16 @@ padding: 10px;
 `;
 
 const SearchWrapper = styled.View`
-  width: 90%;
+  width: 75%;
  margin: 0 16px;
   border-radius: 20px;
   border-width: 1px;
   padding-left: 10px;
   padding-right: 10px;
-  border-color: ${({ theme }: any) => theme.colors.borderGray};
   justify-content: space-between;
   align-items:center;
   flex-direction: row;
+  background-color:#fff;
 `;
 
 const SendMessageWrapper = styled.View`
@@ -116,6 +123,20 @@ const SendMessageWrapper = styled.View`
   margin-bottom: 16px;
   margin-top: auto;
   `;
+
+const styles = StyleSheet.create({
+  shadowBottonContainerStyle: {    //<--- Style without elevation
+    borderWidth: 0,
+    borderRadius: 20,
+    borderColor: '#fff',
+    borderBottomWidth: 2,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+  },
+
+})
 
 const ChatMessage = styled.Text<TextProps>`
   color: ${({ theme }: any) => theme.colors.black};
@@ -131,7 +152,12 @@ const ChatText = styled.Text<TextProps>`
   font-size: ${({ theme }: any) => theme.fontSize.cardDate}px;
   font-weight: 500;
 text-align:  right;`
-;
+  ;
+const TimeAgoTV = styled.Text`
+color:#C7C7C7;
+width:70%;
+text-align:right;
+`;
 
 const ChatNameView = styled.View`
   margin-left: 16px;
@@ -139,6 +165,7 @@ const ChatNameView = styled.View`
 
 const ChatMainView = styled.View`
   width: 100%;
+  padding-right:10px;
 `;
 
 const ProfileImage = styled.Image`
@@ -149,17 +176,13 @@ const ProfileImage = styled.Image`
 `;
 const ChatWrapper = styled.View<TextProps>`
   flex-direction:  ${({ flexDirection }: any) => flexDirection};
-  padding: 10px;
+  padding: 10px 0;
 `;
 
 const ParentWrapper = styled.View`
 height: 100%;
   flex-direction: column;
   display: flex;
+  background-color:#F1F4F6;
 `;
-
-
-function alert(arg0: boolean): ((info: { distanceFromEnd: number; }) => void) | null | undefined {
-    throw new Error("Function not implemented.");
-}
 
